70c38131ea069b2215f983a07ccf5524b62e88d4
Tomasz Blaszczak <tomasz.blaszcak@sky.uk>
2025-06-12 14:34:28 +0200

    feat: Separation of transport layer for Firebolt Native SDKs
    The same transport abstraction is being used across several SDKs, thus is
    separated and put into its own repository.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+case $1 in
+'') ;;
+--) shift;;
+*) SYSROOT_PATH="$1"; shift;;
+esac
+[[ ! -z $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not set" >/dev/stderr; exit 1; }
+[[ -e $SYSROOT_PATH ]] || { echo "SYSROOT_PATH not exist ($SYSROOT_PATH)" >/dev/stderr; exit 1; }
+
+bdir="build"
+rm -rf $bdir
+cmake -B $bdir -DSYSROOT_PATH=$SYSROOT_PATH "$@"
+cmake --build $bdir
+cmake --install $bdir
+
